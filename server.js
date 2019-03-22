b759ca2cb97603f58114b7ba1ae695e04af4fdb7
Parv Jain <parvjn616@gmail.com>
2019-03-23 04:27:08 +0530

    cors problem solved

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
+const cors = require('cors');
 
 const app            = express();
 
 const port = 9000;
 
 const migration = false; //true if you want to migrate csv data to mongodb (one time only)
+
+app.use(cors());
 
 app.use(bodyParser.json({ extended: true }));
 
