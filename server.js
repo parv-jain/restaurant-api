176487a9fd4c3858990c37e317167122419beef4
Parv Jain <parvjn616@gmail.com>
2019-03-23 02:27:37 +0530

    configured for data migration

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const app            = express();
 
 const port = 9000;
 
+const migration = true;
+
 app.use(bodyParser.json({ extended: true }));
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
+  if(migration) {
+    require('./app/migrations')(app, database);
+  }
   require('./app/routes')(app, database);
   app.listen(port, () => {
     console.log('We are live on ' + port);
